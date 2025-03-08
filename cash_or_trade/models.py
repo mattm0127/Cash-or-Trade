9f9fd2c568a7c76aee0ec8e7df58d50c08ba743e
Matt Mennella <matt.mennella@gmail.com>
2025-03-07 19:32:25 -0500

    Fix for database filtering

--- cash_or_trade/models.py
+++ cash_or_trade/models.py
@@ -22,7 +22,7 @@ class Items(db.Model):
     __tablename__ = 'items'
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey(Users.id, ondelete="CASCADE"), nullable=False)
-    type = db.Column(db.String(10), nullable=False)
+    type = db.Column(db.String(15), nullable=False)
     name = db.Column(db.String(50), nullable=False)
     description_id = db.Column(db.Integer)
     price = db.Column(db.Numeric(10, 2), nullable=False)
